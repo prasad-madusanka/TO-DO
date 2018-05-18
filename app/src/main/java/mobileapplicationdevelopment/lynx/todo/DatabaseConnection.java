0e2f3952394ded0bab5ed26a88bc9ef8e70e3412
prasadMadusanka <prasad.mads@gmail.com>
2018-05-18 12:00:45 +0530

    Second Commit

--- app/src/main/java/mobileapplicationdevelopment/lynx/todo/DatabaseConnection.java
+++ app/src/main/java/mobileapplicationdevelopment/lynx/todo/DatabaseConnection.java
@@ -1,4 +1,57 @@
 package mobileapplicationdevelopment.lynx.todo;
 
-public class DatabaseConnection {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseConnection extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "TODO.db";
+    private static final String TABLE_TODO = "todoTable";
+
+    public DatabaseConnection(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String todoTable = "CREATE TABLE "
+                + TABLE_TODO +
+                "(ID INTEGER PRIMARY KEY AUTOINCREMENT," + "DESCRIPTION TEXT,"+" DATE TEXT)";
+        db.execSQL(todoTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String drop="DROP IF TABLE EXISTS ";
+        db.execSQL(drop + TABLE_TODO);
+
+    }
+
+
+
+    protected boolean addData(String date, String todoDescription)throws Exception{
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("DESCRIPTION", date);
+        cv.put("DATE", todoDescription);
+
+        long result = db.insert(TABLE_TODO, null, cv);
+
+        if(result==-1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    protected Cursor readData()throws Exception{
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cur = db.rawQuery("SELECT * FROM "+ TABLE_TODO, null);
+
+        return cur;
+    }
 }
--- app/src/main/java/mobileapplicationdevelopment/lynx/todo/MainActivity.java
+++ app/src/main/java/mobileapplicationdevelopment/lynx/todo/MainActivity.java
@@ -1,13 +1,83 @@
 package mobileapplicationdevelopment.lynx.todo;
 
+import android.database.Cursor;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvTodos;
+    private FloatingActionButton fabAddTodo;
+    private DatabaseConnection dc;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            init();
+            dc =  new DatabaseConnection(getApplicationContext());
+            feedListView();
+        }catch (Exception ex){
+
+        }
+
+        fabAddTodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    boolean insert  = dc.addData("88", "AjA");
+                    feedListView();
+
+                    if (insert) {
+                        toast("Complete");
+
+                    } else {
+                        toast("Wrong");
+                    }
+
+                }catch (Exception ex){
+
+                }
+            }
+        });
+
+
+    }
+
+    private void init(){
+        fabAddTodo = (FloatingActionButton)findViewById(R.id.fabAddTodo);
+        lvTodos = (ListView) findViewById(R.id.lvTodos);
+    }
+
+
+    private void toast(String ms){
+        Toast.makeText(getApplicationContext(), ms, Toast.LENGTH_LONG).show();
+    }
+
+    private void feedListView()throws Exception{
+        Cursor cur  = dc.readData();
+        cur.moveToFirst();
+        ArrayList<String> aList = new ArrayList<String>();
+        if (cur != null) {
+            do {
+                String description = cur.getString(1);
+
+                aList.add(description);
+            } while (cur.moveToNext());
+        }
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, aList);
+        lvTodos.setAdapter(arrayAdapter);
+
     }
 }
